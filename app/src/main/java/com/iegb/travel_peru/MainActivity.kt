54e58a9d7ef510fe957c15950991d9502429aefd
Elian46k <elian46k@kali.Elian46k>
2025-06-03 16:09:28 -0500

    Subida inicial del proyecto travel_peru

---
+++ app/src/main/java/com/iegb/travel_peru/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.iegb.travel_peru
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //startActivity(Intent(applicationContext, SplashActivity::class.java))
+    }
+}
