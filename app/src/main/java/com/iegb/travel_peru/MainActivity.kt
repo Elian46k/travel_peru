63e0630956407bb45249cda311b3311424e1358d
Elian46k <elian46k@gmail.com>
2025-06-08 17:50:10 -0500

    Agrego archivos nuevos

--- app/src/main/java/com/iegb/travel_peru/MainActivity.kt
+++ app/src/main/java/com/iegb/travel_peru/MainActivity.kt
@@ -3,14 +3,22 @@ package com.iegb.travel_peru
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import android.view.View
+import android.widget.ImageView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val imagenes = listOf(
+        R.drawable.ciudad_hz,
+        R.drawable.parque_lama,
+        R.drawable.fondo02_soledad_iglesia_huaraz,
+        R.drawable.fondo03_senorita_hz,
+        R.drawable.fondo04_vinicunca_cusco,
+        R.drawable.fondo05_laguna_hz,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,6 +26,10 @@ class MainActivity : AppCompatActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 
         setContentView(R.layout.activity_main)
+
+        val VisorImagen = findViewById<ImageView>(R.id.Visor_de_Fondo)
+        val imagenAleatoria = imagenes[Random.nextInt(imagenes.size)]
+        VisorImagen.setImageResource(imagenAleatoria)
 
         val BtnEmpezar: Button = findViewById(R.id.btnEmpezar)
 
