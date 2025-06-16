27ecfb0285ec682efb62e4eca1c2da9f8a785b79
Elian46k <elian46k@gmail.com>
2025-06-16 00:47:54 -0500

    funcionamiento de proximos horarios

--- app/src/main/java/com/iegb/travel_peru/Activity/InicioActivity.kt
+++ app/src/main/java/com/iegb/travel_peru/Activity/InicioActivity.kt
@@ -1,17 +1,64 @@
+//package com.iegb.travel_peru.Activity
+//
+//import android.content.Intent
+//import android.graphics.Color
+//import android.os.Bundle
+//import android.view.View
+//import android.widget.Button
+//import androidx.activity.viewModels
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import com.iegb.travel_peru.Adaptadores.AdapViajes
+//import com.iegb.travel_peru.R
+//import com.iegb.travel_peru.VistaModelo.Main_VistaModelo
+//import com.iegb.travel_peru.databinding.ActivityMainBinding
+//import com.iegb.travel_peru.databinding.InicioActivityBinding
+//
+//class InicioActivity : AppCompatActivity() {
+//
+//    private lateinit var binding: InicioActivityBinding
+//    private val viewModel: Main_VistaModelo by viewModels()
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        //agregado
+//        binding = InicioActivityBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        modeloVistaObservacion()
+//
+//        window.statusBarColor = Color.BLACK
+//
+//        setContentView(R.layout.inicio_activity)
+//    }
+//
+//    private fun modeloVistaObservacion() {
+//        viewModel.upcomingTrips.observe(this){
+//            list->
+//            binding.pbProximo.visibility =
+//                if(list.isEmpty())View.GONE else View.GONE
+//            binding.viewProximo.apply{
+//                layoutManager = LinearLayoutManager(
+//                    this@InicioActivity,
+//                    LinearLayoutManager.HORIZONTAL,false
+//                )
+//                adapter = AdapViajes(list)
+//            }
+//        }
+//    }
+//}
+
 package com.iegb.travel_peru.Activity
 
-import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.iegb.travel_peru.Adaptadores.AdapViajes
-import com.iegb.travel_peru.R
 import com.iegb.travel_peru.VistaModelo.Main_VistaModelo
-import com.iegb.travel_peru.databinding.ActivityMainBinding
 import com.iegb.travel_peru.databinding.InicioActivityBinding
 
 class InicioActivity : AppCompatActivity() {
@@ -22,26 +69,24 @@ class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //agregado
         binding = InicioActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         modeloVistaObservacion()
-
+        
         window.statusBarColor = Color.BLACK
-
-        setContentView(R.layout.inicio_activity)
     }
 
     private fun modeloVistaObservacion() {
-        viewModel.upcomingTrips.observe(this){
-            list->
+        viewModel.upcomingTrips.observe(this) { list ->
             binding.pbProximo.visibility =
-                if(list.isEmpty())View.GONE else View.GONE
-            binding.viewProximo.apply{
+                if (list.isEmpty()) View.VISIBLE else View.GONE
+
+            binding.viewProximo.apply {
                 layoutManager = LinearLayoutManager(
                     this@InicioActivity,
-                    LinearLayoutManager.HORIZONTAL,false
+                    LinearLayoutManager.HORIZONTAL,
+                    false
                 )
                 adapter = AdapViajes(list)
             }
